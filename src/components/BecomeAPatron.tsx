393a9392426d9f7f86ccefea4308e59440480ba6
Alexander Alemayhu <alexander@alemayhu.com>
2022-05-08 18:32:10 +0200

    chore: import all of the code from 2anki
    2c686ed0c14a (chore: update cypress workflow, 2022-05-08)

---
+++ src/components/BecomeAPatron.tsx
@@ -0,0 +1,13 @@
+function BecomeAPatron() {
+  return (
+    <a rel="noreferrer" target="_blank" href="https://www.patreon.com/alemayhu">
+      <img
+        alt="Become a patreon button"
+        src="/become_a_patron_button.png"
+        loading="lazy"
+      />
+    </a>
+  );
+}
+
+export default BecomeAPatron;
