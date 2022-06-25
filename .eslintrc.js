079fb498fa50b4646b300add4db479b8bdb2b972
Mads Cordes <Mobilpadde@gmail.com>
2022-06-25 19:52:04 +0200

    prettier!

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-  extends: ['airbnb', 'airbnb-typescript'],
+  extends: ["airbnb", "airbnb-typescript", "prettier"],
   parserOptions: {
-    project: './tsconfig.json',
+    project: "./tsconfig.json",
     tsconfigRootDir: __dirname,
   },
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".tsx"] }],
   },
 };
