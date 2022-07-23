ce604107002fa3978d1ced1b96bdf08f531dcbf1
Alexander Alemayhu <alexander@alemayhu.com>
2022-07-23 14:58:31 +0200

    refactor: use new Sentry project

--- src/lib/handleRedirect.ts
+++ src/lib/handleRedirect.ts
@@ -1,8 +1,8 @@
-import { AxiosResponse } from 'axios';
+import { AxiosResponse } from "axios";
 
 function handleRedirect(response: AxiosResponse): void {
-  if (response.request.responseURL.includes('login')) {
-    window.location.href = '/login#login';
+  if (response.request.responseURL.includes("login")) {
+    window.location.href = "/login#login";
   }
 }
 
