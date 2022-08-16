943894c3376d9857d818f11d41975fafb749bdb4
Alexander Alemayhu <alexander@alemayhu.com>
2022-08-16 09:48:56 +0200

    style: use single quotation for for eslint
    Hopefully we can get this consistent now. Getting annoying with the
    overrites :P

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    quotes: [2, 'single', 'avoid-escape'],
   },
   ignorePatterns: ['.eslintrc.js'],
 };
