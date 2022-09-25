24b6d0f19edd05e2b8f8cd5196363f224ba76083
Alexander Alemayhu <alexander@alemayhu.com>
2022-09-25 14:16:26 +0200

    feat(LearnPage): fix the block stepping

---
+++ src/pages/Learn/helpers/useLearnData.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import Backend from '../../../lib/backend';
+import NotionObject from '../../../lib/interfaces/NotionObject';
+import { ChildrenType } from '../types';
+
+interface LearnData {
+  error: Error | null;
+  page: NotionObject | null;
+  children: ChildrenType;
+}
+
+export const useLearnData = (id: string | null): LearnData => {
+  const [page, setPage] = useState<NotionObject | null>(null);
+  const [error, setError] = useState(null);
+  const [children, setChildren] = useState<ChildrenType>([]);
+  const backend = new Backend();
+
+  useEffect(() => {
+    if (id) {
+      backend
+        .getPage(id)
+        .then((res) => setPage(res))
+        .catch(setError);
+      backend
+        .getBlocks(id)
+        .then((response) => {
+          setChildren(response.results);
+        })
+        .catch(setError);
+    }
+  }, [id]);
+
+  return {
+    error,
+    page,
+    children
+  };
+};
--- src/pages/Learn/index.tsx
+++ src/pages/Learn/index.tsx
@@ -1,71 +1,35 @@
 import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router';
 import { ErrorHandlerType } from '../../components/errors/helpers/types';
 import { Container, PageContainer } from '../../components/styled';
-import Backend from '../../lib/backend';
-import NotionObject from '../../lib/interfaces/NotionObject';
-import { BlockType, ChildrenType } from './types';
+import LoadingPage from '../Loading';
+import { useLearnData } from './helpers/useLearnData';
 import Wrapper from './Wrapper';
 
 interface Props {
   setError: ErrorHandlerType;
 }
 
-const backend = new Backend();
 function LearnPage({ setError }: Props) {
   const [parentId, setParentId] = useState<string | null>(null);
-  const [children, setChildren] = useState<ChildrenType>([]);
-  const [page, setPage] = useState<NotionObject | null>(null);
-  const [block, setBlock] = useState<BlockType>(null);
-  const [grandChildren, setGrandChildren] = useState<ChildrenType>(null);
-  const [location, setLocation] = useState(0);
+  const [index, setIndex] = useState(0);
 
+  const { children, page, error } = useLearnData(parentId);
+  const location = useLocation();
   // Load parent page based on id
   useEffect(() => {
-    const paths = window.location.pathname.split('/');
-    const lastPath = paths[paths.length - 1];
-    setParentId(lastPath);
-    setLocation(1); // temporary until buttons / key presses are implemented
+    setParentId(location.pathname.split('/').at(-1) || null);
   }, []);
 
-  // Get the page meta data
-  useEffect(() => {
-    if (parentId) {
-      backend
-        .getPage(parentId)
-        .then((response) => {
-          setPage(response);
-        })
-        .catch((error) => {
-          setError(error.response.data.message);
-        });
-    }
-  }, [parentId]);
-
-  useEffect(() => {
-    if (page) {
-      backend.getBlocks(page.id).then((response) => {
-        setChildren(response.results);
-      });
-    }
-  }, [page]);
-
-  useEffect(() => {
-    if (children) {
-      setBlock(children[location]);
-    }
-  }, [children, location]);
-
-  useEffect(() => {
-    if (block) {
-      backend.getBlocks(block.id).then((response) => {
-        setGrandChildren(response.results);
-      });
-    }
-  }, [block]);
-
-  if (!parentId || !children) {
-    return <div>insert loading screen.</div>;
+  if (error) {
+    setError(error.toString());
   }
+  if (!parentId || !children) {
+    return <LoadingPage />;
+  }
+
+  const block = children[index - 1];
+
   return (
     <PageContainer>
       <h1>Learn</h1>
@@ -92,13 +56,24 @@ function LearnPage({ setError }: Props) {
                 <h1 className="title">{block.id}</h1>
                 <pre>{JSON.stringify(block, null, 4)}</pre>
                 <hr />
-                <pre>{JSON.stringify(grandChildren, null, 2)}</pre>
               </>
             )}
-            <progress id="file" value={location + 1} max={children.length} />
+            <progress id="file" value={index + 1} max={children.length} />
             <span style={{ fontSize: '11px' }}>
-              {location + 1} /{children.length}
+              {index + 1} /{children.length}
             </span>
+            <button
+              type="button"
+              onClick={() => setIndex(Math.max(index - 1, 0))}
+            >
+              previous
+            </button>
+            <button
+              type="button"
+              onClick={() => setIndex(Math.min(index + 1, children.length - 1))}
+            >
+              Next
+            </button>
           </div>
         </Wrapper>
       </Container>
