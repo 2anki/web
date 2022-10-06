82ba0c36d576e8e71e58d83ca6975be1c19b8ba4
Alexander Alemayhu <alexander@alemayhu.com>
2022-10-06 21:07:14 +0200

    fix: use tracing integration

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css';
 import * as Sentry from '@sentry/react';
-import { BrowserTracing } from '@sentry/tracing';
+import { Integrations } from '@sentry/tracing';
 
 import App from './App';
 
@@ -11,8 +11,8 @@ import LoadingPage from './pages/Loading';
 if (!process.env.REACT_SKIP_SENTRY) {
   Sentry.init({
     dsn: 'https://962b127355704482be99c300f58d00f6@o1284472.ingest.sentry.io/6596166',
-    integrations: [new BrowserTracing()],
-    tracesSampleRate: 1.0,
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0
   });
 }
 
@@ -22,5 +22,5 @@ ReactDOM.render(
       <App />
     </Suspense>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
