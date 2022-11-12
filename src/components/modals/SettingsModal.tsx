232819006bd3163d0a4424e65ca22c2246227d73
Alexander Alemayhu <alexander@alemayhu.com>
2022-11-12 08:21:41 +0100

    style: remove unused link

--- src/components/modals/SettingsModal.tsx
+++ src/components/modals/SettingsModal.tsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Backend from '../../lib/backend';
 import { getTemplateServiceUrl } from '../../lib/backend/getTemplateServiceUrl';
