8cefc6da5ac53c89ff394dcb8bab25e874c60b44
Alexander Alemayhu <alexander@alemayhu.com>
2023-02-06 21:10:48 +0100

    fix: use download only for APKG files

--- src/pages/Search/components/actions/ObjectAction.tsx
+++ src/pages/Search/components/actions/ObjectAction.tsx
@@ -2,21 +2,19 @@
 import { MouseEventHandler } from 'react';
 import { ObjectIconAction } from '../SearchObjectEntry/styled';
 
-interface ObjectActionProps {
+export interface ObjectActionProps {
   url: string;
   image: string;
-  filename?: string;
   onClick?: MouseEventHandler;
 }
 
 export default function ObjectAction({
                                        url,
                                        image,
-                                       filename,
                                        onClick
                                      }: ObjectActionProps) {
   return (
-    <a download={filename} href={url} target="_blank" rel="noreferrer" onClick={onClick}>
+    <a href={url} target="_blank" rel="noreferrer" onClick={onClick}>
       <ObjectIconAction alt="Page action" width="32px" src={image} />
     </a>
   );
