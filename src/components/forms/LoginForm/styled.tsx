3faeec8b8e0e4719fbc3685d798320e82cdebf10
Alexander Alemayhu <alexander@alemayhu.com>
2023-02-19 11:59:16 +0100

    fix: change the hover color on the login submit
    Previously hard to see when tabbing through on a chromebook.

---
+++ src/components/forms/LoginForm/styled.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const FormContainer = styled.div`
+  max-width: 720px;
+  margin: 0 auto;
+`;
+
+export const SubmitButton = styled.button.attrs({ className: 'button is-link is-medium is-pulled-right' })`
+:hover {
+  border: 3px solid #b5b5b5;
+}
+`;
