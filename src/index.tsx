26fb7e9e9444615f6ebfd99c2870819e84529f11
Alexander Alemayhu <alexander@alemayhu.com>
2023-02-26 07:12:25 +0100

    build: resolve type issues
    Fixes the following build issue
    ```
    TS2322: Type 'BrowserTracing' is not assignable to type 'Integration'.
    34
      Types of property 'setupOnce' are incompatible.
    35
        Type '(_: (callback: EventProcessor) => void, getCurrentHub: () => Hub) => void' is not assignable to type '(addGlobalEventProcessor: (callback: EventProcessor) => void, getCurrentHub: () => Hub) => void'.
    36
          Types of parameters '_' and 'addGlobalEventProcessor' are incompatible.
    37
            Types of parameters 'callback' and 'callback' are incompatible.
    38
              Type 'import("/home/runner/work/web/web/node_modules/@sentry/tracing/node_modules/@sentry/types/types/eventprocessor").EventProcessor' is not assignable to type 'import("/home/runner/work/web/web/node_modules/@sentry/types/types/eventprocessor").EventProcessor'.
    39
    
    ```

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css';
+
 import * as Sentry from '@sentry/react';
 import { BrowserTracing } from '@sentry/tracing';
 
@@ -12,7 +13,7 @@ if (!process.env.REACT_SKIP_SENTRY) {
   Sentry.init({
     dsn: 'https://962b127355704482be99c300f58d00f6@o1284472.ingest.sentry.io/6596166',
     integrations: [new BrowserTracing()],
-    tracesSampleRate: 1.0,
+    tracesSampleRate: 1.0
   });
 }
 
