d98a31a66f555dbd5283b47bf7c1df8cfe4eb13e
Alexander Alemayhu <alexander@alemayhu.com>
2023-07-22 13:50:41 +0200

    fix: return error message if error is a string

--- src/components/errors/helpers/getErrorMessage.ts
+++ src/components/errors/helpers/getErrorMessage.ts
@@ -4,6 +4,8 @@ export const getErrorMessage = (error: unknown): string => {
   let msg = 'Unknown error';
   if (error instanceof Error) {
     msg = `<h1 class='title is-4'>${error.message}</h1>`;
+  } else if (typeof error === 'string') {
+    return error;
   }
   return msg;
 };
