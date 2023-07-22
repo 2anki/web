06448b794968f368f3f0736eb04f7623af13fa03
Alexander Alemayhu <alexander@alemayhu.com>
2023-07-22 12:46:45 +0200

    feat(getDownloadFileName): handle server bundles
    The 2anki server can send a zip file with multiple APKG files or one
    APKG file.

--- src/pages/DownloadsPage/helpers/getDownloadFileName.ts
+++ src/pages/DownloadsPage/helpers/getDownloadFileName.ts
@@ -1,5 +1,7 @@
+import { isServerBundle } from './isServerBundle';
+
 export const getDownloadFileName = (fileName: string) => {
-  if (fileName.toLowerCase().endsWith('.apkg')) {
+  if (fileName.toLowerCase().endsWith('.apkg') || isServerBundle(fileName)) {
     return fileName;
   }
   return `${fileName}.apkg`;
