f45e91dda7d862f9edeeb4131195c1268e809a5d
Alexander Alemayhu <alexander@alemayhu.com>
2024-05-08 14:26:12 +0200

    fix: hide sidebar in register page

--- src/components/shared/canShowNavbar.ts
+++ src/components/shared/canShowNavbar.ts
@@ -1,5 +1,6 @@
 const isLoginPage = (path: string) => path.includes('/login');
 const isForgotPasswordPage = (path: string) => path.includes('/forgot');
+const isRegisterPage = (path: string) => path.includes('/register');
 
 export const canShowNavbar = (path: string) =>
-  !isLoginPage(path) && !isForgotPasswordPage(path);
+  !isLoginPage(path) && !isForgotPasswordPage(path) && !isRegisterPage(path);
