c390e3cd56c7e4112fa850ab592ffce232f58f82
Alexander Alemayhu <alexander@alemayhu.com>
2024-05-09 08:28:18 +0200

    add missing ++ (#530)

--- src/pages/HomePage/components/Sections/tier/TierSection.tsx
+++ src/pages/HomePage/components/Sections/tier/TierSection.tsx
@@ -3,28 +3,36 @@ import { TierInfoColumn } from './TierInfoColumn';
 import { getPatreonLink } from '../../../../SettingsPage/getPatreonLink';
 
 function TierSection() {
-  return <div className="container">
-    <section className="section">
-      <div className="container">
-        <div className="columns">
-          <TierInfoColumn title="Anonymous"
-                          description="Works fine for uploads that are under 100 flashcards."
-          />
-          <TierInfoColumn title="Subscriber" description="Create 200 flashcards per upload (100mb)" action={{
-            text: 'Subscribe',
-            link: getSubscribeLink()
-          }} />
-          <TierInfoColumn title="Patreon"
-                          description="No limits on your uploads (up to 9GB++)" action={{
-            text: 'Become a Patron',
-            link: getPatreonLink()
-          }} />
+  return (
+    <div className="container">
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <TierInfoColumn
+              title="Anonymous"
+              description="Works fine for uploads that are under 100 flashcards."
+            />
+            <TierInfoColumn
+              title="Subscriber"
+              description="Create 200++ flashcards per upload (100mb)"
+              action={{
+                text: 'Subscribe',
+                link: getSubscribeLink(),
+              }}
+            />
+            <TierInfoColumn
+              title="Patreon"
+              description="No limits on your uploads (up to 9GB++)"
+              action={{
+                text: 'Become a Patron',
+                link: getPatreonLink(),
+              }}
+            />
+          </div>
         </div>
-      </div>
-    </section>
-
-
-  </div>;
+      </section>
+    </div>
+  );
 }
 
 export default TierSection;
