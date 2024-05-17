5d49bfde697e476e0636921a1bb287dffc0d1d42
Alexander Alemayhu <alexander@alemayhu.com>
2024-05-17 15:54:50 +0200

    feat: add Google sign in (#541)

--- src/components/forms/LoginForm/styled.tsx
+++ src/components/forms/LoginForm/styled.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const SubmitButton = styled.button.attrs({
-  className: 'button is-link is-medium is-pulled-right'
+  className: 'button is-link is-medium is-fullwidth'
 })`
     :hover {
         border: 3px solid #b5b5b5;
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,5 @@
 import LoginForm from '../../components/forms/LoginForm';
-import NavButtonCTA from '../../components/buttons/NavButtonCTA';
 import { Container } from '../../components/styled';
-import TopSection from './TopSection';
 import { ErrorHandlerType } from '../../components/errors/helpers/getErrorMessage';
 
 interface Props {
@@ -9,15 +7,8 @@ interface Props {
 }
 
 export function LoginPage({ setErrorMessage }: Props) {
-  const onClickRegister = () => {
-    window.location.href = '/register';
-  };
   return (
     <Container>
-      <TopSection onClick={onClickRegister}>
-        Don&apos;t have an account?
-        <NavButtonCTA href="/register">Register</NavButtonCTA>
-      </TopSection>
       <LoginForm onError={setErrorMessage} />
     </Container>
   );
