9856090dbcaab2ec3a46642f1cb8c88cb0e15a5b
Alexander Alemayhu <alexander@alemayhu.com>
2024-05-26 08:54:23 +0200

    feat(DebugPage): add button for resetting local storage

--- src/pages/DebugPage/DebugPage.tsx
+++ src/pages/DebugPage/DebugPage.tsx
@@ -1,9 +1,14 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { getKeys } from './helpers/getKeys';
 import { ErrorPresenter } from '../../components/errors/ErrorPresenter';
 
 export function DebugPage() {
   const [show, setShow] = useState(false);
+
+  const resetLocalStorage = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
 
   return (
     <section className="section">
@@ -17,13 +22,24 @@ export function DebugPage() {
               )});\n`
           )}
         </pre>
+        <div className="buttons is-justify-content-space-between mt-2">
+          <div className="control">
+            <button className="button" type="button" onClick={() => setShow(!show)}>
+              {show ? 'Hide' : 'Show'}
+            </button>
+            {
+              show && <ErrorPresenter error={new Error('This is a test error')} />
+            }
 
-        <button type="button" onClick={() => setShow(!show)}>
-          {show ? 'Hide' : 'Show'}
-        </button>
-        {
-          show && <ErrorPresenter error={new Error('This is a test error')} />
-        }
+          </div>
+
+          <div className="control">
+            <button className="button" type="button" onClick={resetLocalStorage}>
+              Reset local storage
+            </button>
+
+          </div>
+        </div>
       </div>
     </section>
   );
