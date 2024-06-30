e6096c960bf1c7d2a2ae4f3729a1b3196e032990
Alexander Alemayhu <alexander@alemayhu.com>
2024-06-30 11:21:29 +0000

    feat: try using USD (#559)

--- src/pages/PricingPage/getSubscribeLink.tsx
+++ src/pages/PricingPage/getSubscribeLink.tsx
@@ -1,4 +1,4 @@
 export const getSubscribeLink = () =>
   process.env.NODE_ENV === 'development' ?
     'https://buy.stripe.com/test_fZebM83k00Rj6PeeUU' :
-    'https://buy.stripe.com/eVadTGcCI6Ny73qfZ0';
+    'https://buy.stripe.com/cN2cPC6ek7RCbjGdQU';
