9eb02058849a1f02793d01721bae8689c42f7b0b
Alexander Alemayhu <alexander@alemayhu.com>
2024-07-14 16:12:31 +0000

    chore: inform users about gmail issues (#566)
    * feat: for now disable gmail login
    
    App is awaiting verification.
    
    * chore: inform users about google sign in issues

--- src/components/forms/WithGoogleLink.tsx
+++ src/components/forms/WithGoogleLink.tsx
@@ -5,10 +5,15 @@ interface WithGoogleLinkProps {
 }
 
 export function WithGoogleLink({ text }: WithGoogleLinkProps) {
-  return <a href={getGoogleSignInUrl()} className="button is-fullwidth">
+  return <div>
+    <div className="notification is-info is-small">
+      <p>Google sign is currently disabled, please register or use forgotten password</p>
+    </div>
+    <a href={getGoogleSignInUrl()} className="button is-fullwidth">
   <span className="icon">
     <i className="fab fa-google" />
   </span>
-    <span>{text}</span>
-  </a>;
+      <span>{text}</span>
+    </a>
+  </div>;
 }
