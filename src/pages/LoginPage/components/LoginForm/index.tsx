45b967c8fd349db93efb1219692b375c2d45d496
Alexander Alemayhu <alexander@alemayhu.com>
2024-07-14 18:04:53 +0200

    feat: for now disable gmail login
    App is awaiting verification.

--- src/pages/LoginPage/components/LoginForm/index.tsx
+++ src/pages/LoginPage/components/LoginForm/index.tsx
@@ -12,6 +12,8 @@ function LoginForm() {
   const { email, password, loading, onSubmit, setEmail, setPassword } =
     useHandleLoginSubmit((e) => setError((e as Error).message));
 
+  const emailGmail = localStorage.getItem('enable-gmail') === 'true';
+
   return (
     <FormContainer>
       <section className="section">
@@ -20,9 +22,9 @@ function LoginForm() {
             <div className="column is-half">
               <TopMessage />
               <h1 className="title is-1">{getVisibleText('navigation.login.title')}</h1>
-              <div className="control">
+              {emailGmail && <div className="control">
                 <WithGoogleLink text={getVisibleText('navigation.login.google')} />
-              </div>
+              </div>}
               <hr />
               <form onSubmit={onSubmit}>
                 <div className="field">
