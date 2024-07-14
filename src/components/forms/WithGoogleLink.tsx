f261c625ae0d184e54dacf0768e1e4df55abe02e
Alexander Alemayhu <alexander@alemayhu.com>
2024-07-14 20:10:15 +0000

    Revert "chore: inform users about gmail issues" (#568)
    This reverts commit 9eb02058849a1f02793d01721bae8689c42f7b0b.

--- src/components/forms/WithGoogleLink.tsx
+++ src/components/forms/WithGoogleLink.tsx
@@ -5,15 +5,10 @@ interface WithGoogleLinkProps {
 }
 
 export function WithGoogleLink({ text }: WithGoogleLinkProps) {
-  return <div>
-    <div className="notification is-info is-small">
-      <p>Google sign is currently disabled, please register or use forgotten password</p>
-    </div>
-    <a href={getGoogleSignInUrl()} className="button is-fullwidth">
+  return <a href={getGoogleSignInUrl()} className="button is-fullwidth">
   <span className="icon">
     <i className="fab fa-google" />
   </span>
-      <span>{text}</span>
-    </a>
-  </div>;
+    <span>{text}</span>
+  </a>;
 }
