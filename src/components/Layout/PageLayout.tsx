348af2707a4ec1517b615fb1bb513c57c8a08229
Alexander Alemayhu <alexander@alemayhu.com>
2024-11-19 05:44:57 +0100

    fix: add type definition for error prop in PageLayout

--- src/components/Layout/PageLayout.tsx
+++ src/components/Layout/PageLayout.tsx
@@ -3,15 +3,17 @@ import { PageContent } from './styled';
 import { ErrorPresenter } from '../errors/ErrorPresenter';
 import NavigationBar from '../NavigationBar/NavigationBar';
 
-interface LayoutProps {
-  error: unknown;
+interface PageLayoutProps {
   children: ReactNode;
+  error?: Error | null;
 }
 
-export function PageLayout({ error, children }: Readonly<LayoutProps>) {
-  return <PageContent>
-    <NavigationBar />
-    {error && <ErrorPresenter error={error} />}
-    {children}
-  </PageContent>;
+export function PageLayout({ children, error }: PageLayoutProps) {
+  return (
+    <PageContent>
+      <NavigationBar />
+      {error && <ErrorPresenter error={error as Error} />}
+      {children}
+    </PageContent>
+  );
 }
