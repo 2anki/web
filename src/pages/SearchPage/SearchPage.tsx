7d37a58ea3c3817c5b63ac7d87a388f4c234d1e8
Alexander Alemayhu <alexander@alemayhu.com>
2025-01-25 06:41:39 +0100

    feat: cleanup the connect to notion page
    This page is generating a lot of unauthorized requests due to the way it
    was rendering the search container. Now it will show a little bit more
    info and stop these request.
    
    This is just first pass and it will be improved in subsequent
    iterations.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import SearchContainer from './components/SearchContainer';
 import useNotionData from './helpers/useNotionData';
 import ConnectNotion from './components/ConnectNotion';
@@ -18,14 +19,23 @@ export function SearchPage({ setError }: SearchPageProps) {
   const { connected, connectionLink } = notionData;
 
   return (
-    <>
-      <ConnectNotion ready={!connected} connectionLink={connectionLink} />
-      <SearchContainer
-        ready={connected}
-        notionData={notionData}
-        backend={get2ankiApi()}
-        setError={setError}
-      />
-    </>
+    <div className="section">
+      {!connected && (
+        <>
+          <h1 className="title is-2 has-text-centered">
+            Connect to Notion or Upload Manually
+          </h1>
+          <ConnectNotion ready={!connected} connectionLink={connectionLink} />
+        </>
+      )}
+      {connected && (
+        <SearchContainer
+          ready={connected}
+          notionData={notionData}
+          backend={get2ankiApi()}
+          setError={setError}
+        />
+      )}
+    </div>
   );
 }
