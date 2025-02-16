9f823ee314263f3145aea43e4baba73a7fc26bf1
Alexander Alemayhu <alexander@alemayhu.com>
2025-02-16 12:26:37 +0100

    fix: inform when email is linked
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- src/lib/hooks/useUserLocals.ts
+++ src/lib/hooks/useUserLocals.ts
@@ -3,12 +3,12 @@ import { useQuery } from 'react-query';
 import { getUserLocals } from '../backend/getUserLocals';
 
 export function useUserLocals() {
-  const { data, isLoading, error, isError } = useQuery({
+  const { data, isLoading, error, isError, refetch } = useQuery({
     queryKey: ['userLocals'],
     queryFn: getUserLocals,
     retry: 3,
     retryDelay: 1000,
   });
 
-  return { data, isLoading, error, isError };
+  return { data, isLoading, error, isError, refetch };
 }
