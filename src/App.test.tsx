d32334b149e3cf0b42561c47db0d4cc1b4abf660
Alexander Alemayhu <alexander@alemayhu.com>
2025-02-16 13:31:40 +0100

    test: update
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App';
 
-test('renders 2anki.net about', () => {
+test('renders 2anki.net title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/What is 2anki/i);
+  const linkElement = screen.getAllByText(/Anki flashcards/i)[0];
   expect(linkElement).toBeInTheDocument();
 });
