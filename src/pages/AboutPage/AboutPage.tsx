826f4dfe62e8be3c638e9aa2dab946b5d34ab108
Alexander Alemayhu <alexander@alemayhu.com>
2025-02-16 12:54:39 +0100

    feat: update about
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -5,7 +5,7 @@ export default function AboutPage() {
   return (
     <PageContainer>
       <div className="content">
-        <div className="hero is-dark mb-6">
+        <div className="hero">
           <div className="hero-body">
             <h1 className="title has-text-centered">About 2anki.net</h1>
             <p className="subtitle has-text-centered">
@@ -29,10 +29,7 @@ export default function AboutPage() {
                   Get Started
                 </a>
               </div>
-            </div>
-
-            <div className="box has-background-dark has-text-light">
-              <p className="is-size-5">
+              <p className="my-4">
                 The goal of the 2anki.net project is to provide a good way to
                 make{' '}
                 <a
@@ -56,7 +53,7 @@ export default function AboutPage() {
                     If you are looking for an Anki or Notion replacement then
                     this project is probably not right for you. We are never
                     going to compete against Anki in this project. We are
-                    building bridges 🌁
+                    building bridges
                   </p>
                   <p className="mt-4">
                     When that is said, if you are not content with Anki, you
@@ -98,26 +95,22 @@ export default function AboutPage() {
 
             <div className="box has-background-dark has-text-light mt-5">
               <h2 className="title is-4">How it works</h2>
-              <div className="steps">
-                <div className="step-item">
-                  <div className="step-marker">1</div>
-                  <div className="step-content">
-                    <p>Create toggle lists in Notion</p>
-                  </div>
-                </div>
-                <div className="step-item">
-                  <div className="step-marker">2</div>
-                  <div className="step-content">
-                    <p>Export and upload to 2anki.net</p>
-                  </div>
-                </div>
-                <div className="step-item">
-                  <div className="step-marker">3</div>
-                  <div className="step-content">
-                    <p>Download and import into Anki</p>
-                  </div>
-                </div>
-              </div>
+              <p className="mb-4">
+                Check out our{' '}
+                <a
+                  href="https://docs.2anki.net/guides/getting-started/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  detailed guide
+                </a>{' '}
+                or follow these simple steps:
+              </p>
+              <ol className="ml-4">
+                <li className="mb-2">Create toggle lists in Notion</li>
+                <li className="mb-2">Export and upload to 2anki.net</li>
+                <li className="mb-2">Download and import into Anki</li>
+              </ol>
               <div className="mt-5">
                 <p>
                   We treat toggle lists on the top level as Anki flashcards. The
@@ -137,76 +130,6 @@ export default function AboutPage() {
                   are, they are enabled by default.
                 </p>
               </div>
-            </div>
-
-            <div className="box has-background-primary-dark has-text-light mt-5">
-              <h2 className="title is-4">Support the Project</h2>
-              <p className="has-text-centered mb-5">
-                This project is brought to you by our amazing{' '}
-                <a
-                  href="https://patreon.com/alemayhu"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  patrons
-                </a>{' '}
-                and{' '}
-                <a
-                  href="https://github.com/sponsors/alemayhu"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  GitHub sponsors
-                </a>{' '}
-                🤩
-              </p>
-              <div className="buttons is-centered">
-                <a
-                  className="button is-primary is-medium"
-                  href="https://github.com/sponsors/alemayhu/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <span className="icon">
-                    <i className="fab fa-github" />
-                  </span>
-                  <span>GitHub Sponsor</span>
-                </a>
-                <a
-                  className="button is-info is-medium"
-                  href="https://patreon.com/alemayhu"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <span className="icon">
-                    <i className="fab fa-patreon" />
-                  </span>
-                  <span>Become a Patron</span>
-                </a>
-                <a
-                  className="button is-medium"
-                  href="https://paypal.me/alemayhu"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <span className="icon">
-                    <i className="fab fa-paypal" />
-                  </span>
-                  <span>PayPal</span>
-                </a>
-              </div>
-            </div>
-
-            <div className="box has-background-dark has-text-light mt-5">
-              <h2 className="title is-4">License</h2>
-              <p>
-                Unless otherwise specified in the source: The code is licensed
-                under the MIT License, Copyright (c) 2020-2024,{' '}
-                <a href="https://alemayhu.com" target="_blank" rel="noreferrer">
-                  Alexander Alemayhu
-                </a>
-                .
-              </p>
             </div>
           </div>
         </div>
