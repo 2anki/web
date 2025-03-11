5ee66317c6b3b0ec0c582adeb82c800143064b32
Alexander Alemayhu <alexander@alemayhu.com>
2025-03-11 19:36:11 +0100

    chore: show user local in debug

--- src/pages/DebugPage/DebugPage.tsx
+++ src/pages/DebugPage/DebugPage.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { getKeys } from './helpers/getKeys';
 import { ErrorPresenter } from '../../components/errors/ErrorPresenter';
+import { useUserLocals } from '../../lib/hooks/useUserLocals';
 
 export function DebugPage() {
   const [show, setShow] = useState(false);
+  const { data } = useUserLocals();
 
   const resetLocalStorage = () => {
     localStorage.clear();
@@ -14,6 +16,10 @@ export function DebugPage() {
     <section className="section">
       <h1 className="title">Debug page</h1>
       <div className="container">
+        <h2 className="subtitle">User Locals</h2>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+
+        <h2 className="subtitle mt-4">Local Storage</h2>
         <pre>
           {getKeys(localStorage).map(
             (key) =>
@@ -24,20 +30,26 @@ export function DebugPage() {
         </pre>
         <div className="buttons is-justify-content-space-between mt-2">
           <div className="control">
-            <button className="button" type="button" onClick={() => setShow(!show)}>
+            <button
+              className="button"
+              type="button"
+              onClick={() => setShow(!show)}
+            >
               {show ? 'Hide' : 'Show'}
             </button>
-            {
-              show && <ErrorPresenter error={new Error('This is a test error')} />
-            }
-
+            {show && (
+              <ErrorPresenter error={new Error('This is a test error')} />
+            )}
           </div>
 
           <div className="control">
-            <button className="button" type="button" onClick={resetLocalStorage}>
+            <button
+              className="button"
+              type="button"
+              onClick={resetLocalStorage}
+            >
               Reset local storage
             </button>
-
           </div>
         </div>
       </div>
