2687f33c69246338f14749fc818fbd8bcca59a00
Alexander Alemayhu <alexander@alemayhu.com>
2025-04-11 20:41:16 +0200

    Fix tests by wrapping UploadForm in BrowserRouter

--- src/pages/UploadPage/components/UploadForm/UploadForm.test.tsx
+++ src/pages/UploadPage/components/UploadForm/UploadForm.test.tsx
@@ -1,25 +1,34 @@
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import UploadForm from './UploadForm';
 
 describe('UploadForm', () => {
   test('download button is light by default', () => {
     const { container } = render(
-      <UploadForm setErrorMessage={(error) => fail(error)} isLoggedIn={false} />
+      <BrowserRouter>
+        <UploadForm setErrorMessage={(error) => fail(error)} isLoggedIn={false} />
+      </BrowserRouter>
     );
     expect(container.querySelector('.button.cta.is-light')).toBeInTheDocument();
   });
 
   test('no null classes', () => {
     const { container } = render(
-      <UploadForm setErrorMessage={(error) => fail(error)} isLoggedIn={false} />
+      <BrowserRouter>
+        <UploadForm setErrorMessage={(error) => fail(error)} isLoggedIn={false} />
+      </BrowserRouter>
     );
     expect(container.querySelector('.null')).toBeNull();
   });
 
   test('download button is disabled', () => {
-    render(<UploadForm setErrorMessage={(error) => fail(error)} isLoggedIn={false} />);
+    render(
+      <BrowserRouter>
+        <UploadForm setErrorMessage={(error) => fail(error)} isLoggedIn={false} />
+      </BrowserRouter>
+    );
     expect(document.querySelector('.button.cta')).toBeDisabled();
   });
 });
