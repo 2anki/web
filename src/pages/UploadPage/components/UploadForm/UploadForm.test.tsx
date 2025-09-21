87887a6b5e21d6ba59de243fa471e7d2639aed8d
Alexander Alemayhu <alexander@alemayhu.com>
2025-09-21 20:46:51 +0200

    fix: resolve build issues
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- src/pages/UploadPage/components/UploadForm/UploadForm.test.tsx
+++ src/pages/UploadPage/components/UploadForm/UploadForm.test.tsx
@@ -1,25 +1,26 @@
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { vi } from 'vitest';
 
 import UploadForm from './UploadForm';
 
 describe('UploadForm', () => {
   test('download button is light by default', () => {
     const { container } = render(
-      <UploadForm setErrorMessage={(error) => fail(error)} />
+      <UploadForm setErrorMessage={vi.fn()} />
     );
     expect(container.querySelector('.button.cta.is-light')).toBeInTheDocument();
   });
 
   test('no null classes', () => {
     const { container } = render(
-      <UploadForm setErrorMessage={(error) => fail(error)} />
+      <UploadForm setErrorMessage={vi.fn()} />
     );
     expect(container.querySelector('.null')).toBeNull();
   });
 
   test('download button is disabled', () => {
-    render(<UploadForm setErrorMessage={(error) => fail(error)} />);
+    render(<UploadForm setErrorMessage={vi.fn()} />);
     expect(document.querySelector('.button.cta')).toBeDisabled();
   });
 });
