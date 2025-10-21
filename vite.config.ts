710e838159c7574ae37ef8f8b9067c52600368da
Alexander Alemayhu <alexander@alemayhu.com>
2025-10-21 08:45:51 +0200

    chore: remove unused VitePWA import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 import legacy from '@vitejs/plugin-legacy';
 import svgr from 'vite-plugin-svgr';
 import path from 'node:path';
